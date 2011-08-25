e9db295c8ba029d75b7007cfbf6c5f9722f1450a
d.syph.3r <d.syph.3r@googlemail.com>
2011-08-25 20:54:37 +0100

    added function to get latlng component from result

--- Entity/Location.php
+++ Entity/Location.php
@@ -79,24 +79,51 @@ class Location
      * may return more than 1 match for a search. The $match param can
      * be used to specify the result to return
      *
-     * @param   int     $match      Result match to return
+     * @param   int     $match      Result match to return (indexes start at 0)
      * @return  array               Key/Value address components
      */
-    public function getAddress($match = 1)
+    public function getAddressComponents($match = 0)
     {
         $matches = json_decode($this->getResult(), true);
         
-        $components = array();
-        if (isset($matches[$match]))
+        if (false === isset($matches[$match]))
         {
-            foreach ($matches[$match]['address_components'] as $component)
-            {
-                $type = $component['types'][0];
-                $components[$type] = $component['long_name'];
-            }
+            return false;
         }
         
+        $components = array();
+        foreach ($matches[$match]['address_components'] as $component)
+        {
+            $type = $component['types'][0];
+            $components[$type] = $component['long_name'];
+        }
+
         return $components;
+    }
+    
+    /**
+     * Get the latlng components for a Geocoded result. Geocoding service
+     * may return more than 1 match for a search. The $match param can
+     * be used to specify the result to return
+     *
+     * @param   int     $match      Result match to return (indexes start at 0)
+     * @return  array               Key/Value latlng component
+     */
+    public function getLatLng($match = 0)
+    {
+        $matches = json_decode($this->getResult(), true);
+        
+        if (false === isset($matches[$match]))
+        {
+            return false;
+        }
+        
+        $components = array();
+        $components['lat'] = $matches[$match]['geometry']['location']['lat'];
+        $components['lng'] = $matches[$match]['geometry']['location']['lng'];
+        
+        return $components;
+        
     }
     
     /**
